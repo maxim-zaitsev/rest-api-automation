eac741b8f734c39e9b8080593573a2d1a76b5841
vmred <vladdavidchenko@gmail.com>
2019-10-01 13:07:25 +0300

    Add validate json schema

--- src/core/service.py
+++ src/core/service.py
@@ -1,4 +1,9 @@
+import json
+from os.path import join
+from pathlib import Path
+
 import pytest
+from jsonschema import validate
 
 from src.core.api import API
 
@@ -7,6 +12,7 @@ class Service(API):
     def __init__(self):
         super().__init__()
         self._fail = ''
+        self._schemas_dir = '{}/services/'.format(Path(__file__).parent.parent)
 
     def is_failed(self):
         if self._fail is not '':
@@ -18,3 +24,21 @@ class Service(API):
     def verify(self, condition):
         if not condition:
             self.fail('--> {} condition is not True \n'.format(condition))
+
+    def _set_schemas_dir(self, value):
+        self._schemas_dir += value
+
+    def get_schemas_dir(self):
+        return self._schemas_dir
+
+    def _load_json_schema(self, filename):
+
+        absolute_path = join(self.get_schemas_dir(), filename)
+
+        with open(absolute_path) as schema_file:
+            return json.loads(schema_file.read())
+
+    def verify_json_schema(self, data, schema_file):
+
+        schema = self._load_json_schema(schema_file)
+        return validate(data, schema)
--- src/services/reqres/reqres.py
+++ src/services/reqres/reqres.py
@@ -5,3 +5,4 @@ class Reqres(Service):
     def __init__(self):
         super().__init__()
         self._set_hostname('http://reqres.in')
+        self._set_schemas_dir('reqres/schemas/')
